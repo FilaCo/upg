3ab4cf381252dd5f120e62f6d5a7f543655606c4
FilaCo <awesome.chekushka@gmail.com>
2021-06-07 20:23:56 +0500

    feat(pass_generator): initial commit
    Initial commit for password generator. Add necessary files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='upg',
+
+    version='0.1.0',
+
+    description='TBD',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/FilaCo/upg',
+
+    author='FilaCo',
+    author_email='awesome.chekushka@gmail.com',
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+
+    python_requires='>=3.6, <4',
+)
